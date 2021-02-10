2ba80e1df7c681ba8231a563a8c6e815e9b53754
Freddy Drennan <fdrennan@Freddys-MacBook-Pro-2.local>
2021-02-09 23:00:30 -0700

    update

--- pre-install.sh
+++ pre-install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
@@ -28,18 +28,12 @@ sudo apt-get install -y \
     libpq-dev \
     docker-ce \
     docker-ce-cli \
-    containerd.io 
+    containerd.io
 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# sudo groupadd docker
+sudo groupadd docker | echo already in group
 sudo usermod -aG docker $USER
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo echo "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" >> /etc/apt/sources.list
-
-sudo apt-get update -y \
-    r-base \
-    r-base-dev

73451bc15ce54f9403906509e1df72904033c32a
Freddy Drennan <fdrennan@Freddys-MacBook-Pro-2.local>
2021-02-16 20:27:40 -0700

    update

---
+++ airflow/scripts/submissions_count.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+
+conn = psycopg2.connect(dbname=os.getenv("POSTGRES_DB"),
+                        user=os.getenv("POSTGRES_USER"),
+                        password=os.getenv("POSTGRES_PASSWORD"),
+                        port=os.getenv("POSTGRES_PORT"),
+                        host=os.getenv("POSTGRES_HOST"))
+
+try:
+    cur = conn.cursor()
+    cur.execute("refresh materialized view submissions_count ")
+    conn.commit()
+    cur.close()
+except psycopg2.Error as e:
+    pass
